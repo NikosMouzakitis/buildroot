e36303483d45de4604d20518d5095e49fdaa0266
NikosMouzakitis <mzktsn@gmail.com>
2021-11-07 23:29:15 +0200

    working notes
    Signed-off-by: NikosMouzakitis <mzktsn@gmail.com>

--- run_edk2_firmware.sh
+++ run_edk2_firmware.sh
@@ -3,11 +3,13 @@
 cp /home/nicko/BUSYBOX_INSTALLATION/edk2/Build/OvmfX64/DEBUG_GCC48/FV/OVMF* ovmf-edk2/
 
 machine="q35,smm=on"\
-
+#--enable-kvm  \
 #run the virtual machine,with SMM enabled using the firmware created by the EDK2 codebase.
 qemu-system-x86_64 \
-	-M $machine --enable-kvm  -m 4096 \
-	-smp 2 \
+	-M $machine \
+       	-m 4096 \
+	-smp 4  \
+	-enable-kvm \
 	-bios ovmf-edk2/OVMF.fd \
 	-drive file=output/images/rootfs.ext2,if=virtio,format=raw \
 	-kernel output/images/bzImage \
@@ -16,7 +18,6 @@ qemu-system-x86_64 \
 	-drive if=pflash,format=raw,unit=0,file=ovmf-edk2/OVMF_CODE.fd,readonly=on \
 	-drive if=pflash,format=raw,unit=1,file=ovmf-edk2/OVMF_VARS.fd \
 	-global ICH9-LPC.disable_s3=1 \
-	-nic user,model=virtio,hostfwd=tcp::22222-:22 \
+	-nic user,model=virtio,hostfwd=tcp::22223-:22 \
 	-chardev stdio,id=deb \
 	-device isa-debugcon,iobase=0x402,chardev=deb \
-
